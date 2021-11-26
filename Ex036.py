41ae910d73511f22c29c52a0de2a49813df650b3
Lucas Seridonio <lucas.seridonio@nappsolutions.com>
2021-11-26 11:41:03 -0300

    Alteração no texto

--- Ex036.py
+++ Ex036.py
@@ -1,4 +1,4 @@
-casa = float(input('\nQual valor da casaa? '))
+casa = float(input('\nQual valor da house? '))
 salario = float(input('\nQual valor do salario? '))
 anos = int(input('\nEm Quantos anos vai pagar? '))
 
